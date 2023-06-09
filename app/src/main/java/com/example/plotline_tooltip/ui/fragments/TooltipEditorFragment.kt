3bff0a8b1b161cc8ca3db3df6d879d3c39b7766b
Ankit-jailwal <jailwalankit@gmail.com>
2023-06-10 01:44:51 +0530

    Removed boiler plate code from Tooltip editor fragment

--- app/src/main/java/com/example/plotline_tooltip/ui/fragments/TooltipEditorFragment.kt
+++ app/src/main/java/com/example/plotline_tooltip/ui/fragments/TooltipEditorFragment.kt
@@ -10,39 +10,34 @@ import androidx.fragment.app.Fragment
 import android.widget.Button
 import android.widget.PopupWindow
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.plotline_tooltip.R
+import com.example.plotline_tooltip.databinding.FragmentTooltipEditorBinding
 import com.example.plotline_tooltip.ui.tooltip.TooltipHelper
 import com.example.plotline_tooltip.ui.viewmodels.TooltipViewModel
 
 class TooltipEditorFragment : Fragment() {
-
+    private lateinit var binding: FragmentTooltipEditorBinding
     private lateinit var sharedViewModel: TooltipViewModel
-    private lateinit var button: Button
-    private lateinit var tooltipHelper: TooltipHelper
-    private lateinit var tooltipHandler: Handler
-    private lateinit var tooltipRunnable: Runnable
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_tooltip_editor, container, false)
-
         sharedViewModel = ViewModelProvider(requireActivity()).get(TooltipViewModel::class.java)
+        binding = FragmentTooltipEditorBinding.inflate(inflater, container, false)
 
-        button = view.findViewById(R.id.btnPrintData)
-        tooltipHelper = TooltipHelper(requireContext())
-        tooltipHandler = Handler(Looper.getMainLooper())
-        tooltipRunnable = Runnable {
-            tooltipHelper.hideTooltip()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnPrintData.setOnClickListener {
+
         }
-        button.setOnClickListener {
-
-        }
-
-
-        return view
     }
 
 }
