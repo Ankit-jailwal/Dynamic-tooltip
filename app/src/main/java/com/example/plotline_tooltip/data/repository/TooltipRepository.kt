a61dfcaf0b975fdf27afead53221d28e9cc6e71e
Ankit-jailwal <jailwalankit@gmail.com>
2023-06-10 03:17:14 +0530

    Created tooltip repository

--- app/src/main/java/com/example/plotline_tooltip/data/repository/TooltipRepository.kt
+++ app/src/main/java/com/example/plotline_tooltip/data/repository/TooltipRepository.kt
@@ -1,15 +1,16 @@
-//import androidx.lifecycle.LiveData
-//import com.example.plotline_tooltip.data.db.TooltipDao
-//import com.example.plotline_tooltip.data.model.TooltipDataEntity
+import androidx.lifecycle.LiveData
+import com.example.plotline_tooltip.data.db.TooltipDao
+import com.example.plotline_tooltip.data.model.TooltipDataEntity
 
-//class TooltipDataRepository(private val tooltipDataDao: TooltipDao) {
-//    val allTooltipData: LiveData<List<TooltipDataEntity>> = tooltipDataDao.getAllTooltipData()
-//
-//    suspend fun insertTooltipData(tooltipData: TooltipDataEntity) {
-//        tooltipDataDao.insertTooltipData(tooltipData)
-//    }
-//
-//    suspend fun deleteTooltipData(buttonId: String) {
-//        tooltipDataDao.deleteTooltipData(buttonId)
-//    }
-//}
+class TooltipDataRepository(private val tooltipDao: TooltipDao) {
+    val allTooltipData: LiveData<List<TooltipDataEntity>> = tooltipDao.getAllTooltipData()
+
+    val allTooltiData: LiveData<List<TooltipDataEntity>> = tooltipDao.getAllTooltipData()
+    suspend fun insertTooltipData(tooltipData: TooltipDataEntity) {
+        tooltipDao.insertTooltipData(tooltipData)
+    }
+
+    suspend fun updateTooltipData(tooltipData: TooltipDataEntity) {
+        tooltipDao.updateTooltipData(tooltipData)
+    }
+}
