7833e9fa389777626f4b51e78bc998fc51e3315f
Ankit-jailwal <jailwalankit@gmail.com>
2023-06-09 10:52:09 +0530

    Created Tooltip DAO

---
+++ app/src/main/java/com/example/plotline_tooltip/data/db/TooltipDao.kt
@@ -0,0 +1,20 @@
+package com.example.plotline_tooltip.data.db
+
+import TooltipDataEntity
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TooltipDao {
+    @Query("SELECT * FROM tooltip_data")
+    fun getAllTooltipData(): LiveData<List<TooltipDataEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTooltipData(tooltipData: TooltipDataEntity)
+
+    @Query("DELETE FROM tooltip_data WHERE buttonId = :buttonId")
+    suspend fun deleteTooltipData(buttonId: String)
+}
